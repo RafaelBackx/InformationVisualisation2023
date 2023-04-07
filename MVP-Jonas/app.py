e6673c1146c65e0e32782693d91336ca31e8bdad
RafaelBackx <rafaelbackx@live.be>
2023-04-07 14:46:59 +0200

    minimal changes to the MVP of Jonas to remove parallel earths and only allow a specific zoom

--- MVP-Jonas/app.py
+++ MVP-Jonas/app.py
@@ -5,14 +5,14 @@ import dash_leaflet.express as dlx
 from dash import Dash, html, dcc, Input, Output
 from pandas_geojson import to_geojson
 
-disaster_data = pd.read_csv("../Data/Natural-Disasters.csv", delimiter=";")
+disaster_data = pd.read_csv("../Data/Preprocessed-Natural-Disasters.csv", delimiter=";")
 map_data = disaster_data[disaster_data["Latitude"].notnull() & disaster_data["Longitude"].notnull()]
-map_data = map_data[disaster_data["Year"] == 2000]
+map_data = map_data[disaster_data["Start Year"] == 2000]
 events_geojson = to_geojson(df=map_data, lat="Latitude", lon="Longitude", properties=["Dis No"])
 
 app = Dash(__name__)
 
-map = dl.Map(children=[
+map = dl.Map(maxBounds=[[-90,-180],[90,180]], maxBoundsViscosity=1.0, maxZoom=18, minZoom=2, bounceAtZoomLimits=True, children=[
     dl.TileLayer(), 
     # https://datahub.io/core/geo-countries#resource-countries
     dl.GeoJSON(url="https://pkgstore.datahub.io/core/geo-countries/countries/archive/23f420f929e0e09c39d916b8aaa166fb/countries.geojson", id="countries", options={"style": {"color": "transparent"}}),
