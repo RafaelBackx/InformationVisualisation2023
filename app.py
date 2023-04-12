32c26bb87fdac79e61cd2da24ca19185a521df38
unknown <jonasdeboeck@hotmail.be>
2023-04-12 14:18:51 +0200

    Fixed modal year, fixed main page graphs, fixed modal map, colour scheme of graphs = colour legend map

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ import plotly.express as px
 import util
 from server import app, server
 
+EVENT_COLOURS = {
+    "Meteorological": "#ABA4A4",
+    "Geophysical": "#C95B20",
+    "Hydrological": "#0EB7E3",
+    "Climatological": "#FFEE00"
+}
+
 ns = Namespace("dashExtensions", "default")
 
 disaster_data = pd.read_csv("Data/Preprocessed-Natural-Disasters.csv", delimiter=";")
@@ -84,6 +91,17 @@ worldwide_toggle_buttons = html.Div(children=[
                                         value='deaths'),
                                     ], className="radio-group")
 
+def generate_affected_graph(current_year, current_toggle):
+    affected_data = disaster_data[disaster_data["Start Year"] <= current_year]
+    affected_data = affected_data.groupby(["Start Year", "Disaster Subgroup"], as_index=False).sum(numeric_only=True)
+    column_map = {
+        'deaths': 'Total Deaths',
+        'injuries': 'No Injured',
+        'homeless': 'No Homeless'
+    }
+    column = column_map[current_toggle]
+    return px.line(affected_data, "Start Year", column, color="Disaster Subgroup", color_discrete_map=EVENT_COLOURS)
+
 ############
 #          #
 #  LAYOUT  #
@@ -127,10 +145,10 @@ app.layout = html.Div(children=[
 
 # app.layout = html.Div(children=[map, world_slider, animation_button, animation_interval, worldwide_events_distribution, worldwide_affected_graph, html.Div(id="popup")])
 
-def generate_country_popup(country,df):
+def generate_country_popup(country, current_year):
     country_name = country["properties"]["ADMIN"]
     country_iso = country["properties"]["ISO_A3"] # Can be useful to do lookups
-    country_data = util.filter_map_events(df, {"ISO": country_iso})
+    country_data = util.filter_map_events(disaster_data, {"ISO": country_iso, "Start Year": current_year})
     popup = dbc.Modal(
         children=[
             dbc.ModalHeader(dbc.ModalTitle(country_name)),
@@ -153,11 +171,11 @@ def generate_country_popup(country,df):
                     ],
                 style={"width": "100%", "height": "50vh", "margin": "auto", "display": "block"}, 
                 id="detailed-map"),
-                dcc.Slider(min=1960, max=2023, step=1, value=2021, marks=None,tooltip={'placement': 'bottom', 'always_visible': True}, id='country-year-slider'),
+                dcc.Slider(min=1960, max=2023, step=1, value=current_year, marks=None,tooltip={'placement': 'bottom', 'always_visible': True}, id='country-year-slider'),
                 html.Div(children=[
-                    dcc.Graph(id='gdp-graph',figure=px.line(df,x='Start Year', y="Reconstruction Costs, Adjusted ('000 US$)")),
+                    dcc.Graph(id='gdp-graph',figure=px.line(map_data,x='Start Year', y="Reconstruction Costs, Adjusted ('000 US$)")),
                     html.Div(children= [
-                        dcc.Graph(id='affected-graph',figure=px.line(df,x='Start Year', y="Reconstruction Costs, Adjusted ('000 US$)")),
+                        dcc.Graph(id='affected-graph',figure=px.line(map_data,x='Start Year', y="Reconstruction Costs, Adjusted ('000 US$)")),
                         html.Div(
                                 [
                                     dbc.RadioItems(
@@ -193,14 +211,14 @@ def generate_country_popup(country,df):
 ###############
 
 # Open popup when click on country
-@app.callback(Output("popup", "children"),[Input('countries', 'n_clicks')], [State("countries", "click_feature")], prevent_initial_call=True)
-def country_click(_n_clicks,feature):
+@app.callback(Output("popup", "children"),[Input('countries', 'n_clicks')], [State("countries", "click_feature"), State("world-year-slider", "value")], prevent_initial_call=True)
+def country_click(_n_clicks, feature, current_year):
     if feature is not None:
-       return generate_country_popup(feature, map_data) # disable the interval because otherwise it draws on the other map
+       return generate_country_popup(feature, current_year) # disable the interval because otherwise it draws on the other map
     
 @app.callback(Output('animation-interval', 'disabled'),
               Input('animation-button', 'n_clicks'),
-              State('animation-interval','disabled'))
+              State('animation-interval','disabled'), prevent_initial_call=True)
 def animate_slider(n_clicks, animation_status):
     return not animation_status
 
@@ -208,7 +226,7 @@ def animate_slider(n_clicks, animation_status):
               Input('animation-interval','n_intervals'),
               State('world-year-slider', 'value'),
               State('world-year-slider', 'max'),
-              State('world-year-slider', 'min'))
+              State('world-year-slider', 'min'), prevent_initial_call=True)
 def update_slider(_n_clicks, current_slider_value, max, min):
     if (current_slider_value < max):
         return current_slider_value + 1
@@ -218,38 +236,39 @@ def update_slider(_n_clicks, current_slider_value, max, min):
 @app.callback(Output('events','data'),
               Output('worldwide-events-dist', 'figure'),
               Output('worldwide-affected-graph', 'figure'),
-              Input('world-year-slider','value'))
-def worldwide_slider_change(value):
+              [Input('world-year-slider','value'),
+               Input('worldwide-affected-buttons', 'value')])
+def worldwide_slider_change(current_year, current_toggle):
     # Update general map
-    map_data = util.filter_map_events(disaster_data, {"Start Year": value})
+    map_data = util.filter_map_events(disaster_data, {"Start Year": current_year})
 
     # Update event distribution graph
     event_count = map_data.groupby("Disaster Subgroup").count().reset_index()
-    event_dist_fig = px.bar(event_count, x="Dis No", y="Disaster Subgroup")
+    event_count["colour"] = event_count["Disaster Subgroup"].map(EVENT_COLOURS)
+    event_dist_fig = px.bar(event_count, x="Dis No", y="Disaster Subgroup", color="Disaster Subgroup", color_discrete_map=EVENT_COLOURS)
 
-    # Update affected graph
-    events_affected = disaster_data[disaster_data["Start Year"] <= value]
-    events_affected = events_affected.groupby(["Start Year", "Disaster Subgroup"], as_index=False).sum(numeric_only=True)
-    affected_fig = px.line(events_affected, "Start Year", "Total Deaths", color="Disaster Subgroup")
+    # Update affected
+    affected_fig = generate_affected_graph(current_year, current_toggle)
+    
     return util.convert_events_to_geojson(map_data), event_dist_fig, affected_fig
 
-@app.callback([Output('gdp-graph','figure'), Output('affected-graph','figure')],[Input('country-year-slider','value'),Input('graph-toggle-buttons','value')], State("countries", "click_feature"))
-def country_slider_change(value,toggle_value,country):
+@app.callback([Output('country-events', 'data'),
+               Output('gdp-graph','figure'), 
+               Output('affected-graph','figure')],
+               [Input('country-year-slider','value'),
+                Input('graph-toggle-buttons','value')], 
+                State("countries", "click_feature"))
+def country_slider_change(current_year,toggle_value,country):
     country_code = country["properties"]["ISO_A3"]
     data = util.filter_map_events(disaster_data, {'ISO': country_code})
+    # Update map
+    map_data = util.filter_map_events(data, {"Start Year": current_year})
     # update graphs gpd graph
-    years = list(range(1960,value+1))
-    country_gdp_data = util.get_gdp_data(gdp_data,data[data["Start Year"] <= value],years,country_code)
+    years = list(range(1960,current_year+1))
+    country_gdp_data = util.get_gdp_data(gdp_data,data[data["Start Year"] <= current_year],years,country_code)
     gdp_fig = px.line(country_gdp_data, 'Start Year', 'share')
 
-    #update injuries graph
-    column_map = {
-        'deaths': 'Total Deaths',
-        'injuries': 'No Injured',
-        'homeless': 'No Homeless'
-    }
-    column = column_map[toggle_value]
-    data = util.filter_map_events(disaster_data, {'ISO': country_code})
-    data = data.groupby(['Start Year', 'Disaster Subgroup'], as_index=False).sum(numeric_only=True)
-    affected_fig = px.line(data[data['Start Year'] <= value],'Start Year',column,color='Disaster Subgroup')
-    return gdp_fig, affected_fig
+    # update affected graph
+    affected_fig = generate_affected_graph(current_year, toggle_value)
+
+    return util.convert_events_to_geojson(map_data), gdp_fig, affected_fig
